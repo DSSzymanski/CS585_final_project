ea1ec2055350559c4fdd4a293fdf98765fca6828
DSSzymanski <dszymanski@umass.edu>
2018-11-06 15:39:05 -0500

    added method for creating data file for ratings

--- scraper.py
+++ scraper.py
@@ -15,16 +15,16 @@ for people who haven't used before
 """
 ratings = namedtuple('rating_percents', ['approval', 'disapproval', 'no_opinion'])
 
-#url, beautiful soup object, and the correct table
-page = requests.get("https://news.gallup.com/poll/203198/presidential-approval-ratings-donald-trump.aspx")
-soup = BeautifulSoup(page.content, 'html.parser')
-table = soup.find('table', attrs={"aria-labelledby":"caption-20181105122501"})
-
 """
 getApproval Dict
 returns a dictionary of strings of weeks mapped to rating named tuple
 """
 def getApprovalList():
+    #url, beautiful soup object, and the correct table
+    page = requests.get("https://news.gallup.com/poll/203198/presidential-approval-ratings-donald-trump.aspx")
+    soup = BeautifulSoup(page.content, "html.parser")
+    table = soup.find("table", attrs={"aria-labelledby":"caption-20181105122501"})
+    
     approvalList = []
     
     rows = table.find_all('tr')
@@ -41,3 +41,20 @@ def getApprovalList():
             weeks_rating = ratings(data[0].string,data[1].string,data[2].string)
             approvalList.insert(0,(week.string,weeks_rating))
     return approvalList
+
+#takes approvalList entry, converts to string to store in file
+def tupleToString(lEntry):
+    retStr = lEntry[0] + "\t"
+    for item in lEntry[1]:
+        retStr += item + "\t"
+    return retStr
+    
+def generateRatingDataFile():
+    approvalList = getApprovalList()
+    
+    #converted every entry to list format
+    listOfStrings = [tupleToString(x) + "\n" for x in approvalList]
+    
+    fh = open("data/rating_data.txt", "w")    
+    fh.writelines(listOfStrings)
+    fh.close()
