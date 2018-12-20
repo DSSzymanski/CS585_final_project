bd96eb2360499f204bd853e13169ec2f64ae3a83
Derek Chiang <b00611007@gmail.com>
2018-12-19 21:19:28 -0500

    Modify the CNN framework.

--- src/char_tensor_cnn.py
+++ src/char_tensor_cnn.py
@@ -11,10 +11,14 @@ class CharTensorConvNet(nn.Module):
         super(CharTensorConvNet, self).__init__()
         self.conv1 = nn.Sequential(nn.Conv2d(char_length, n_conv_filters, kernel_size=(input_dim, 3)),
                                    nn.ReLU())  # (N, 50, 28)
-        self.conv2 = nn.Sequential(nn.Conv1d(n_conv_filters, n_conv_filters, kernel_size=3), nn.ReLU())
-        self.conv3 = nn.Sequential(nn.Conv1d(n_conv_filters, n_conv_filters, kernel_size=3), nn.ReLU())
+        self.conv2 = nn.Sequential(nn.Conv1d(n_conv_filters, n_conv_filters, kernel_size=3, padding=1),
+                                   nn.ReLU())
+        self.conv3 = nn.Sequential(nn.Conv1d(n_conv_filters, n_conv_filters, kernel_size=3, padding=1),
+                                   nn.ReLU())
+        self.conv4 = nn.Sequential(nn.Conv1d(n_conv_filters, n_conv_filters, kernel_size=3, padding=1),
+                                   nn.ReLU(), nn.MaxPool1d(2))
 
-        dimension = n_conv_filters * (word_length - 6)
+        dimension = n_conv_filters * int((word_length - 2) / 2)
         self.fc1 = nn.Sequential(nn.Linear(dimension, n_fc_neurons), nn.ReLU(), nn.Dropout(0.5))
         self.fc2 = nn.Sequential(nn.Linear(n_fc_neurons, n_fc_neurons), nn.ReLU(), nn.Dropout(0.5))
         self.fc3 = nn.Linear(n_fc_neurons, n_classes)
@@ -24,6 +28,7 @@ class CharTensorConvNet(nn.Module):
         output = output.squeeze()
         output = self.conv2(output)
         output = self.conv3(output)
+        output = self.conv4(output)
 
         output = output.view(output.size(0), -1)
         output = self.fc1(output)
